de177379e8b6bba90399a659be68142b2adb9cf6
xxinchao <xxinchao@gmail.com>
2012-04-24 18:11:50 +0800

    "this is a Fisher-Yates shuffling algorithm "

---
+++ shuffling.py
@@ -0,0 +1,13 @@
+#Fisher-Yates shuffle algo
+import random
+a=[1,2,3,4,5]
+len(a)
+def fyshuffling(a):
+    for i in range(len(a)-1,0,-1):
+        j=random.randint(0,i)
+        a[i],a[j]=a[j],a[i]
+
+if __name__=='__main__':
+    fyshuffling(a)
+    print a
+
